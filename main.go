f0debe3f3aa45eacb04716feecb1b5121593153e
amir sobhan ahmadi <amirsobhanahmadi@gmail.com>
2025-08-06 14:43:49 +0330

    add DB_ENGINE field to the .env file and i check the DB_ENGINE field in main.go file to perform actions based on the DB_ENGINE field

--- main.go
+++ main.go
@@ -1,6 +1,11 @@
 package main
 
 import (
+	"log"
+	"os"
+	"strings"
+
+	"github.com/joho/godotenv"
 	"github.com/sobhaann/echo-taskmanager/handlers"
 	"github.com/sobhaann/echo-taskmanager/storage"
 
@@ -8,11 +13,25 @@ import (
 )
 
 func main() {
-	db := storage.ConnectPostgres()
+	godotenv.Load()
+	dbEngineType := os.Getenv("DB_ENGINE")
 
-	postgres := handlers.NewTaskHandler(db)
+	if strings.ToLower(dbEngineType) == "gorm" {
+		db := storage.ConnectPostgresGORM()
 
-	postgres.Run()
+		postgres := handlers.NewTaskHandlerGORM(db)
+
+		postgres.GormRun()
+	} else if strings.ToLower(dbEngineType) == "pq" {
+		db := storage.ConnectPostgresPQ()
+
+		postgres := handlers.NewTaskHandlerPQ(db)
+
+		postgres.PQRun()
+	} else {
+		log.Fatal("the database engine should be `gorm` or `pq`!!!!")
+	}
+
 }
 
 // --TODO--
